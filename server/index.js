84fcc0e84a106e4eea1555fb8600056c87f7cbe4
Murali Bandaru <murali@x0pa.com>
2026-01-08 00:07:07 +0530

    seventh commit

--- server/index.js
+++ server/index.js
@@ -40,7 +40,14 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("/*", cors(corsOptions));
+
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 
 app.use(express.json());
 
